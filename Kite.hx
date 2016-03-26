a6fc31490c499848f98bc9760d41cf2bb2e64c48
Aaron Santiago <aaronsantiago@gmail.com>
2016-03-26 04:51:04 -0400

    Add kites

---
+++ Kite.hx
@@ -0,0 +1,68 @@
+
+import h3d.col.Point;
+import h3d.prim.UV;
+
+class Kite extends CustomPolygon {
+
+    public function new( x = 1., y = 1., z = 1., my = .5)
+    {
+        var p = [
+            new Point( 0,  -y/2,      0),
+            new Point( x/2, y*my-y/2, 0),
+            new Point(-x/2, y*my-y/2, 0),
+            new Point( 0,   y*my-y/2, z/2),
+            new Point( 0,   y*my-y/2, -z/2),
+            new Point( 0,   y/2,      0),
+        ];
+        var idx = new hxd.IndexBuffer();
+        idx.push(0); idx.push(1); idx.push(3);
+        idx.push(0); idx.push(2); idx.push(3);
+        idx.push(0); idx.push(1); idx.push(4);
+        idx.push(0); idx.push(2); idx.push(4);
+        idx.push(5); idx.push(1); idx.push(3);
+        idx.push(5); idx.push(2); idx.push(3);
+        idx.push(5); idx.push(1); idx.push(4);
+        idx.push(5); idx.push(2); idx.push(4);
+
+        super(p, idx);
+
+        normals = [
+            new Point( 0,  -1,      0),
+            new Point( 1, 0, 0),
+            new Point(-1, 0, 0),
+            new Point( 0,   0, 1),
+            new Point( 0,   0, -1),
+            new Point( 0,   1,      0),
+        ];
+    }
+
+    override function addUVs() {
+        // unindex();
+
+        var z = new UV(0, 1);
+        var x = new UV(1, 1);
+        var y = new UV(0, 0);
+        var o = new UV(1, 0);
+
+        uvs = [
+            z, x, o,
+            z, o, y,
+            x, z,
+        ];
+
+        originalUvs = [];
+
+        for( uv in uvs )
+        {
+            originalUvs.push(uv);
+        }
+
+
+        // var r = new Point(1,0,0);
+        // var g = new Point(0,1,0);
+        // var b = new Point(0,0,1);
+        // var x = new Point(1,1,1);
+
+        // colors = [r,g,b,x,r,g,b,x];
+    }
+}
--- Main.hx
+++ Main.hx
@@ -12,19 +12,20 @@ class Main extends hxd.App {
     var pulseCounter: Float = 0;
     var obj1 : Mesh;
     var obj2 : Mesh;
-    var prim : Cube;
+    var prim:Kite;
 
     override function init() {
         motion.actuators.SimpleActuator.stage_onEnterFrame();
         TweenX.updateMode = UpdateModeX.MANUAL;
-        prim = new Cube();
+        prim = new Kite(.6,2,1,.9);
+        // prim = new Cube();
 
         // translate it so its center will be at the center of the cube
         // prim.translate( -0.5, -0.5, -0.5);
         prim.x += 1;
 
         // unindex the faces to create hard edges normals
-        // prim.unindex();
+        prim.unindex();
 
         // add face normals
         prim.addNormals();
@@ -108,8 +109,8 @@ class Main extends hxd.App {
             pulseCounter -= 150;
         }
 
-        prim.x += .01 * dt;
-        prim.z += .01 * dt;
+        // prim.x += .01 * dt;
+        // prim.z += .01 * dt;
         prim.needsUpdate = true;
 
         prim.update();
