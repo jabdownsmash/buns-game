9d4ffae13a4670da2bfb30d9026ee35cd131b85b
Aaron Santiago <aaronsantiago@gmail.com>
2016-04-02 06:22:49 -0400

    Move scene-related code out of Engine

--- engine/Engine.hx
+++ engine/Engine.hx
@@ -12,17 +12,9 @@ class Engine extends hxd.App
 
     public var input:Input;
 
-    var time : Float = 0.;
-    var light:h3d.scene.DirLight;
-
     override function init()
     {
         TweenX.updateMode = UpdateModeX.MANUAL;
-
-        light = new h3d.scene.DirLight(new h3d.Vector(0.6, 0.5, -0.5), s3d);
-        // light.enableSpecular = true;
-
-        s3d.lightSystem.ambientLight.set(0.3, 0.3, 0.3);
 
         s2d.addEventListener(handleKeys);
 
--- engine/misato/GameScene.hx
+++ engine/misato/GameScene.hx
@@ -5,7 +5,7 @@ import h3d.scene.Scene;
 
 import engine.objects.Object;
 
-class GameScene
+class GameScene implements engine.GameScene
 {
 
     public var name:String;
@@ -13,15 +13,20 @@ class GameScene
     public var apiObject:Object;
     public var s3d:Scene;
 
+    var light:h3d.scene.DirLight;
+
     public function new(engine:Engine)
     {
         apiObject = new Object(engine.s3d);
-        this.s3d = engine.s3d;
+        s3d = engine.s3d;
     }
 
     public function init()
     {
+        light = new h3d.scene.DirLight(new h3d.Vector(0.6, 0.5, -0.5), s3d);
+        // light.enableSpecular = true;
 
+        s3d.lightSystem.ambientLight.set(0.3, 0.3, 0.3);
     }
 
     public function update(dt:Float)
